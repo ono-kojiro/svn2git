edf04043f4724de2c0c7b173023aad6dafabbc6f
Kojiro ONO <ono.kojiro@gmail.com>
2020-09-30 22:43:38 +0900

    update git-svn-update.py

--- git-svn-update.py
+++ git-svn-update.py
@@ -25,7 +25,7 @@ def get_command_output(cmd) :
         if not line and proc.poll() is not None:
             break
 
-def init_git_svn(url, ext_dir) :
+def init_git_svn(url, rev, ext_dir) :
     cmd = 'git -C {0} init'.format(ext_dir, url)
     print('CMD : {0}'.format(cmd))
     for line in get_command_output(cmd) :
@@ -36,46 +36,88 @@ def init_git_svn(url, ext_dir) :
     for line in get_command_output(cmd) :
         print(line)
     
-    cmd = 'git -C {0} svn fetch'.format(ext_dir)
-    print('CMD : {0}'.format(cmd))
-    for line in get_command_output(cmd) :
-        print(line)
+    #cmd = 'git -C {0} svn fetch --revision {1}'.format(ext_dir, rev)
+    #print('CMD : {0}'.format(cmd))
+    #for line in get_command_output(cmd) :
+    #    print(line)
 
-def checkout_git(url, ext_dir) :
-    print('{0}, {1}'.format(url, ext_dir))
+def checkout_git(url, rev, ext_dir) :
+    print('CHECKOUT_GIT : {0}, {1}'.format(url, ext_dir))
     if not os.path.isdir(ext_dir) :
-        print("INFO : no such working directory, '{0}'".format(ext_dir))
         if ext_dir != '.' and not os.path.isdir(ext_dir) :
-            print("'make ext_dir, {0}'".format(ext_dir))
             os.makedirs(ext_dir)
-
-        init_git_svn(url, ext_dir)
+        print('INFO : initialize git repository, {0}, {1}'.format(ext_dir, url))
+        init_git_svn(url, rev, ext_dir)
 
     else :
         print('directory exists, {0}'.format(ext_dir))
 
     pass
 
+def split_externals(work_dir, line) :
+    url = ''
+    rev = ''
+    ext_dir = ''
+
+    pattern = '({0})([^\s]+?)(@(\d+))?\s+([^\s]+)'.format(work_dir)
+    m = re.search(pattern, line)
+    if m :
+        url = m.group(2)
+        rev = m.group(4)
+        ext_dir = m.group(5)
+        print('{0}, {1}, {2}'.format(url, rev, ext_dir))
+
+    return url, rev, ext_dir
+
 def main() :
+    git_exclude = '.git/info/exclude'
+    fp = open(git_exclude, mode='a', encoding='utf-8')
+
+    work_dir = ''
+
     cmd = 'git svn show-externals'
 
+    items = []
+
     for line in get_command_output(cmd):
+        print("LINE : '{0}'".format(line))
         if line == '' :
             continue
 
         m = re.search(r'^# (.*/)$', line)
         if m :
+            work_dir = m.group(1)
             continue
 
-        m = re.search(r'([^\s]+)\s+(.+)', line)
-        if m :
-            ext_dir = '.' + m.group(1)
-            url = m.group(2)
-            
-            cmd = 'git -C {0} svn rebase'.format(ext_dir)
-            print('{0}'.format(cmd))
-            subprocess.run(cmd, shell=True, encoding='utf-8')
+        url, rev, ext_dir = split_externals(work_dir, line)
+
         
+        if url != '' and ext_dir != '':
+            ext_dir = '.' + work_dir + ext_dir
+            
+            fp.write('{0}\n'.format(ext_dir))
+
+            item = {
+                'url' : url,
+                'rev' : rev,
+                'ext_dir' : ext_dir
+            }
+
+            items.append(item)
+
+    for item in items :
+        url = item['url']
+        rev = item['rev']
+        ext_dir = item['ext_dir']
+
+        checkout_git(url, rev, ext_dir)
+
+        #cmd = 'git -C {0} svn rebase'.format(ext_dir)
+        #print('{0}'.format(cmd))
+        #subprocess.run(cmd, shell=True, encoding='utf-8')
+    
+    fp.close()
+
 if __name__ == "__main__" :
     main()
 
